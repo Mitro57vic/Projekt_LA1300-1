fecde8720e2b24851c8d7f1b37f629c4a2cb9828
justus <justus@UELI-MAURERS-LA>
2022-12-13 09:53:59 +0100

    Projektdateien hinzufügen.

---
+++ Pong-game/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Pong_game
+{
+    partial class Pong
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PlayerBar = new System.Windows.Forms.PictureBox();
+            this.ComBar = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.PlayerScoreLabel = new System.Windows.Forms.Label();
+            this.ComScoreLabel = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ComBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayerBar
+            // 
+            this.PlayerBar.BackColor = System.Drawing.Color.Lime;
+            this.PlayerBar.Location = new System.Drawing.Point(14, 209);
+            this.PlayerBar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.PlayerBar.Name = "PlayerBar";
+            this.PlayerBar.Size = new System.Drawing.Size(24, 152);
+            this.PlayerBar.TabIndex = 0;
+            this.PlayerBar.TabStop = false;
+            // 
+            // ComBar
+            // 
+            this.ComBar.BackColor = System.Drawing.Color.Lime;
+            this.ComBar.Location = new System.Drawing.Point(877, 209);
+            this.ComBar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ComBar.Name = "ComBar";
+            this.ComBar.Size = new System.Drawing.Size(24, 152);
+            this.ComBar.TabIndex = 1;
+            this.ComBar.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Lime;
+            this.Ball.Location = new System.Drawing.Point(445, 271);
+            this.Ball.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(22, 25);
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
+            // 
+            // PlayerScoreLabel
+            // 
+            this.PlayerScoreLabel.AutoSize = true;
+            this.PlayerScoreLabel.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.PlayerScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PlayerScoreLabel.Location = new System.Drawing.Point(267, 12);
+            this.PlayerScoreLabel.Name = "PlayerScoreLabel";
+            this.PlayerScoreLabel.Size = new System.Drawing.Size(29, 32);
+            this.PlayerScoreLabel.TabIndex = 3;
+            this.PlayerScoreLabel.Text = "0";
+            // 
+            // ComScoreLabel
+            // 
+            this.ComScoreLabel.AutoSize = true;
+            this.ComScoreLabel.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ComScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ComScoreLabel.Location = new System.Drawing.Point(621, 12);
+            this.ComScoreLabel.Name = "ComScoreLabel";
+            this.ComScoreLabel.Size = new System.Drawing.Size(29, 32);
+            this.ComScoreLabel.TabIndex = 4;
+            this.ComScoreLabel.Text = "0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(914, 601);
+            this.Controls.Add(this.ComScoreLabel);
+            this.Controls.Add(this.PlayerScoreLabel);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.ComBar);
+            this.Controls.Add(this.PlayerBar);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Pong";
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ComBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox Playewr;
+        private PictureBox PlayerBar;
+        private PictureBox ComBar;
+        private PictureBox Ball;
+        private Label PlayerScoreLabel;
+        private Label ComScoreLabel;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
---
+++ Pong-game/Form1.cs
@@ -0,0 +1,114 @@
+using System.Numerics;
+
+namespace Pong_game
+{
+    public partial class Pong : Form
+    {
+        public Pong()
+        {
+            InitializeComponent();
+        }
+        
+        //Variablen deklarieren
+        bool goUp;
+        bool goDown;
+        int Speed = 5;
+        int ballX = 7;
+        int ballY = 7; 
+        int Playerscore = 0;
+        int ComScore = 0;
+        int MidX = 408;
+        int MidY = 245;
+
+        public void BallMovement() //Bewegung vom Ball
+        {
+            Ball.Top -= ballY;
+            Ball.Left -= ballX;
+
+            ComBar.Top -= Speed;
+            ComBar.Top = Ball.Top - 50;
+
+            if (Ball.Left < 0)
+            {
+                Ball.Left = MidX - Ball.Width / 2;
+                Ball.Top = MidY - Ball.Height / 2;
+                ComScore++;
+            }
+
+            if (Ball.Left + Ball.Width > ClientSize.Width)
+            {
+                Ball.Top = MidY;
+                Ball.Left = MidX;
+                Playerscore++;
+            }
+
+            if (Ball.Top < 0 || Ball.Top + Ball.Height > ClientSize.Height)
+            {
+                ballY = -ballY;
+            }
+
+            if (Ball.Bounds.IntersectsWith(PlayerBar.Bounds) || Ball.Bounds.IntersectsWith(ComBar.Bounds))
+            {
+                ballX = -ballX;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e) //Tastendruck erkennen
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)//Tasten loslassen erkennen
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+        }
+
+        public void BarControlls() //Bars bewegen
+        {
+            if (goUp == true && PlayerBar.Top > 0)
+            {
+                PlayerBar.Top -= 8;
+            }
+
+            if (goDown == true && PlayerBar.Top + PlayerBar.Height < ClientSize.Height)
+            {
+                PlayerBar.Top += 8;
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e) //Main
+        {
+            PlayerScoreLabel.Text = "" + Playerscore;
+            ComScoreLabel.Text = "" + ComScore;
+
+                BallMovement();
+                BarControlls();
+
+            if (Playerscore >= 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Congratulations, you have won this game!");
+            }
+
+            if (ComScore >= 10)
+            {
+                gameTimer.Stop();
+                MessageBox.Show("You lost this game :(");
+            }
+        }
+    }
+}
